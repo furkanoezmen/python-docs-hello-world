8ab241e2ba570faa15d8f0f61395e333f2e50104
furkanoezmen <furkanoezmen@web.de>
2021-07-12 16:09:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+
+from flask import Flask, current_app, flash, jsonify, make_response, redirect, request, url_for
+from argparse import ArgumentParser
+from zlib import decompress
+from base45 import b45decode
+from cose.messages import CoseMessage
+from cbor2 import loads
+import json
+import argparse
 app = Flask(__name__)
+
+wsgi_app = app.wsgi_app
 
 @app.route("/")
 def hello():
